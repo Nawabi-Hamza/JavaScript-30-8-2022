18d4fe342792bbb43210ed0d2c39231212d90716
Hamza-Nawabi <111171188+Nawabi-Hamza@users.noreply.github.com>
2022-08-30 00:42:37 -0700

    new tasks

--- js3.js
+++ js3.js
@@ -1,12 +1,12 @@
 
 
 
-function change(f){
-    f=(5*(f-32))/9;
+// function change(f){
+//     f=(5*(f-32))/9;
     
-    return f;
+//     return f;
 
-}
-// document.write(change(98.6));
+// }
+// // document.write(change(98.6));
 
-document.getElementById('de').innerHTML="to the tempreture is " + Math.round(change(89))  + "C";
+// document.getElementById('de').innerHTML="to the tempreture is " + Math.round(change(89))  + "C";
---
+++ js4.js
@@ -0,0 +1,12 @@
+
+function click1(){
+    document.getElementById('ch').src="download.jpg";
+    document.getElementById('hide1').style="display:none;";
+    document.getElementById('hide2').style="display:block;";
+
+}
+function click2(){
+    document.getElementById('ch').src="sd.jpg";
+    document.getElementById('hide2').style="display:none;";
+    document.getElementById('hide1').style="display:block;";
+}
